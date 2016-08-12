b142fa625acd32c4385191cfbd7a7dfb5d0253db
jiyinyiyong <jiyinyiyong@gmail.com>
2016-08-12 15:11:33 +0800

    make editor div flexible

--- compiled/src/cirru_editor/comp/editor.cljs
+++ compiled/src/cirru_editor/comp/editor.cljs
@@ -12,12 +12,11 @@
 (def style-editor
  {:min-height "200px",
   :background-color (hsl 200 90 3),
-  :width "100%",
+  :flex 1,
   :padding "8px 8px 8px 8px",
   :display "flex",
   :position "relative",
-  :flex-direction "column",
-  :height "100%"})
+  :flex-direction "column"})
 
 (def style-box
  {:flex 1, :padding "100px 0 200px 0", :overflow-y "auto"})
