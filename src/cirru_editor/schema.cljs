10323f591c0a103faa34e31d13867ee5aeae63e2
jiyinyiyong <jiyinyiyong@gmail.com>
2019-05-18 15:58:55 +0800

    add colors to literal values; alpha release

--- src/cirru_editor/schema.cljs
+++ src/cirru_editor/schema.cljs
@@ -1,4 +1,7 @@
 
 (ns cirru-editor.schema )
 
-(def store {:tree [["demo" ["cute" ["cute"]] "demo"] ["a"]], :focus [], :clipboard []})
+(def store
+  {:tree ["defn" "get-something" [] ["[]" "1" "-1" "\"2" "true" "nil" ":k"]],
+   :focus [],
+   :clipboard []})
